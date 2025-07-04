5db01c7f125525191842b06d0a7175e202875856
sekigan98 <sekigan98@gmail.com>
2025-07-03 21:34:37 -0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const numeros = [
-    "https://wa.me/5492231111111",
-    "https://wa.me/5492232222222"
+    "https://wa.me/5491169613906",
+    "https://wa.me/5491169613856",
+    "https://wa.me/5491169619639"
   ];
   const elegido = numeros[Math.floor(Math.random() * numeros.length)];
   const mensaje = "?text=Hola!%20Estoy%20interesado%20en%20más%20info";
