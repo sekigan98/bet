b3bfbacd838c1535b55bfb46195e3ebd28c93ca1
sekigan98 <sekigan98@gmail.com>
2025-07-12 19:59:58 -0300

    Actualizar redirect.js

--- redirect.js
+++ redirect.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const numeros = [
-    "https://wa.me/5491169613906",
-    "https://wa.me/5491169613856"
+    "https://wa.me/5491169640767",
+    "https://wa.me/5491169640722",
+    "https://wa.me/5491169628112",
+    "https://wa.me/5491169636057",
+    "https://wa.me/5491169628243",
+    "https://wa.me/5491169636653",
+    "https://wa.me/5491169616397"
   ];
   const elegido = numeros[Math.floor(Math.random() * numeros.length)];
   const mensaje = "?text=¡Hola!%20Me%20gustaría%20más%20información%20sobre%20el%20bono%20que%20vi.%20¿Cómo%20sigo?";
