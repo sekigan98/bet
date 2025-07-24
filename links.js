14861c36d8c500c57b8ad8633e2f8f1c47982dbc
sekigan98 <sekigan98@gmail.com>
2025-07-24 18:27:36 -0300

    Update links.js

--- links.js
+++ links.js
@@ -1,17 +1,17 @@
 const links = [
 
-"https://wa.me/5491169628596?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
+"https://wa.me/5491169628596?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias", //wp3
 
-"https://wa.me/5491169606562?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
+"https://wa.me/5491169606562?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias", //wp6
 
-"https://wa.me/5491169648712?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
+"https://wa.me/5491169648712?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias", //wp5
 
-"https://wa.me/5491169616397?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
+"https://wa.me/5491169616397?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias", //wp7
 
-"https://wa.me/5491169651371?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
+"https://wa.me/5491169650944?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias", //wp4
 
-"https://wa.me/5491169640722?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
+"https://wa.me/5491169640722?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias", //wp2
 
-"https://wa.me/5491169640767?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias"
+"https://wa.me/5491169640767?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias" //wp1
 
 ];
