878f78e3b4782197709919d514e3e8bd55cc2748
sekigan98 <sekigan98@gmail.com>
2025-07-24 15:31:35 -0300

    Update links.js

--- links.js
+++ links.js
@@ -1,16 +1,16 @@
 const links = [
 
-"https://wa.me/5491169616397?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
-
-"https://wa.me/5491169640722?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
-
-"https://wa.me/5491169639518?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
+"https://wa.me/5491169628596?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
 
 "https://wa.me/5491169606562?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
 
-"https://wa.me/5491169616617?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
-
 "https://wa.me/5491169648712?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
+
+"https://wa.me/5491169616397?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
+
+"https://wa.me/5491169651371?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
+
+"https://wa.me/5491169640722?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias",
 
 "https://wa.me/5491169640767?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario%20gracias"
 
